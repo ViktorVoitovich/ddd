bcdbd6a135e61e8b37af9fc73bd14533d049be91
Viktor Voitovich <voitovich@viktor.local>
2021-12-26 18:55:57 +0300

    feat: create Note Value Objects

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
 
 import 'failures.dart';
 
@@ -19,4 +20,48 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
   }
 
   return right(input);
+}
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  }
+
+  return left(ValueFailure.exceedingLength(
+    faildValue: input,
+    max: maxLength,
+  ));
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  }
+
+  return left(ValueFailure.empty(faildValue: input));
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (!input.contains('\n')) {
+    return right(input);
+  }
+
+  return left(ValueFailure.multiline(faildValue: input));
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+  KtList<T> input,
+  int maxLength,
+) {
+  if (input.size <= maxLength) {
+    return right(input);
+  }
+
+  return left(ValueFailure.listToLong(
+    faildValue: input,
+    max: maxLength,
+  ));
 }
